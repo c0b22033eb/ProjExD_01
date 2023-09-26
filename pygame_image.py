b5ed34f651e723125134c3d0db70f4f562a90873
ogasho24 <onome24@icloud.com>
2023-09-26 15:35:53 +0900

    練習２

--- pygame_image.py
+++ pygame_image.py
@@ -3,11 +3,14 @@ import pygame as pg
 
 
 def main():
+
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    tori_img3 = pg.image.load("fig/3.jpg")
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
